2c92dc8564120033a76f2e2188a8e2529a5fd9b8
Dilaxer <60285937+Dilaxer@users.noreply.github.com>
2020-05-07 18:09:14 +0300

    newest

---
+++ Chameleon/Assets/bugCatchingTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class bugCatchingTimer : MonoBehaviour
+{
+
+    public TextMeshProUGUI timer;
+    public Animator blackscreen;
+
+    IEnumerator Start()
+    {
+        for (int i = 8; i >= 0; i--)
+        {
+            timer.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        blackscreen.Play("FadeOutBlackScreen");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(8);
+
+    }
+
+    
+}
